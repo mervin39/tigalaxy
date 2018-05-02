e6472c55ed404754f335aa7436b31adf7e6c0fa0
Peter Stocker <peter.stocker@gmail.com>
2018-05-02 11:18:50 +0100

    preload systems json

---
+++ public/js/preload.js
@@ -0,0 +1,36 @@
+// This file is run first
+// and preloads tile data into global ti4 object.
+
+var ti4 = {};
+
+(function(){
+  var isLoaded = {
+    json: false
+  };
+  
+  fetch('json/systems.json', {method: 'get'})
+    .then(function(res){
+      res.json().then(function(data){
+        ti4.systems = data;
+        console.log('json loaded')
+        isLoaded.json = true;
+        checkEverythingIsLoaded();
+      });
+    })
+    .catch(function(err){
+      console.log('error', err);
+    });
+    
+  function checkEverythingIsLoaded(){
+    var finishedLoading = true;
+    for ( let task in isLoaded ) {
+      if ( !isLoaded[task] ) {
+        finishedLoading = false;
+      }
+    }
+    if ( finishedLoading ) {
+      // run main site;
+      console.log('all loaded')
+    }
+  }
+}())
