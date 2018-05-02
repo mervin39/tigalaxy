1abd5e42e8cb64e787fa48f7ec92092b590cddb8
Peter Stocker <peter.stocker@gmail.com>
2018-05-02 09:59:42 +0100

    added ejs view engine

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 var router = require('./routes/router');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use('/', router.create);
 
